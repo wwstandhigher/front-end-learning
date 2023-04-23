123d9f122c08eef4df50156bd6043505057c69e1
wwstandhigher <130266372+wwstandhigher@users.noreply.github.com>
2023-04-23 23:25:48 +0800

    Add files via upload

---
+++ h1.js
@@ -0,0 +1,16 @@
+let obj1 = new Object();
+obj.name = "lyj";
+obj.age = 20;
+
+let obj2 = {
+    name: 'ljp',
+    age: 19
+}
+
+function person() {
+    l.name = 'pjh';
+    l.age = 19;
+}
+let obj3 = new person();
+
+console.log(obj1, obj2, obj3);
